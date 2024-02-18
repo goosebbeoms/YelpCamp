65a0607587b3793ac524d68b0d23a442935f0059
goosebbeoms <seo980916@gmail.com>
2024-02-18 22:14:54 +0900

    2024-02-18 22:15

--- models/campground.js
+++ models/campground.js
@@ -8,7 +8,7 @@ const ImageSchema = new Schema({
 })
 
 ImageSchema.virtual('thumbnail').get(function() {
-    this.url.replace('/upload', '/upload/w_200');
+    return this.url.replace('/upload', '/upload/w_200');
 })
 
 const CampgroundSchema = new Schema({
